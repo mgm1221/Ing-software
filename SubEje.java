bf7a9aad6e19b222ec78076d0c10a39244844fb3
DayanSC <dasaez2020@inf.udec.cl>
2023-11-18 22:42:55 -0300

    Create SubEje.java

---
+++ SubEje.java
@@ -0,0 +1,17 @@
+public class SubEJe{
+    private int id_sub_eje
+    private string nombre
+    public SubEje(int id_sub_eje, string nombre){
+        this.id_sub_eje = id_sub_eje;
+        this.nombre = nombre;
+    }
+    public int get_Id_subEje(){
+        return get_Id_subEje;
+    }
+    public string getNombre(){
+        return nombre;
+    }
+    public porcentajeODS(int numODS){
+        
+    }
+}
