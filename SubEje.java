9725832c4b645e4e10f722f00cc846a25b253a36
DayanSC <dasaez2020@inf.udec.cl>
2023-11-18 22:48:12 -0300

    Update SubEje.java

--- SubEje.java
+++ SubEje.java
@@ -1,12 +1,12 @@
 public class SubEJe{
-    private int id_sub_eje
-    private string nombre
+    private int id_sub_eje;
+    private string nombre;
     public SubEje(int id_sub_eje, string nombre){
         this.id_sub_eje = id_sub_eje;
         this.nombre = nombre;
     }
     public int get_Id_subEje(){
-        return get_Id_subEje;
+        return id_sub_eje;
     }
     public string getNombre(){
         return nombre;
