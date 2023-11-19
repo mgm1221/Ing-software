046bb6d80147e6f4e1006fdc70c08dd0710c6d9b
crmunozb <113199318+crmunozb@users.noreply.github.com>
2023-11-18 22:39:58 -0300

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,20 @@
+public class Item {
+    private int idItem;
+    private String nombre;
+
+    public int getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(int idItem) {
+        this.idItem = idItem;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
