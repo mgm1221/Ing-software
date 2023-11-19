c1c17ba41998dc0c34d982e63a6977c53bbab6f9
crmunozb <113199318+crmunozb@users.noreply.github.com>
2023-11-18 22:31:47 -0300

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,29 @@
+public class Persona {
+    private int idPersona;
+    private String nombre;
+    private String cargo;
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
