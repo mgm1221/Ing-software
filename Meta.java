27cc395734806edcecc5453331645d2885fa355c
martin <martingarcesm@gmail.com>
2023-11-18 22:35:21 -0300

    terminada meta

--- Meta.java
+++ Meta.java
@@ -1,3 +1,15 @@
 public class Meta {
-    
+    private int id_meta;
+    private String descripcion;
+
+    public Meta(int i, String d){
+        this.id_meta = i;
+        this.descripcion = d;
+    }
+    public int get_Id_meta(){
+        return this.id_meta;
+    }
+    public  String get_descipcion(){
+        return this.descripcion;
+    }
 }
