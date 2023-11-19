b50a1e6e4c57477d0f91900a10b5846393e9be2c
crmunozb <113199318+crmunozb@users.noreply.github.com>
2023-11-18 22:31:01 -0300

    Add files via upload

---
+++ Contribucion.java
@@ -0,0 +1,21 @@
+public class Contribucion {
+    private String descripcion;
+    private Date fecha;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+}
+
